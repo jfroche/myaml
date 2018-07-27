1bcdcec68d3b70ff4a9725a3c8593e43864cc452
Jean-Francois Roche <jfroche@affinitic.be>
2018-07-27 16:59:30 +0200

    Add nix config

---
+++ shell.nix
@@ -0,0 +1,36 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, aeson, base, bytestring, directory, filepath
+      , optparse-applicative, stdenv, text, unordered-containers, vector
+      , yaml
+      }:
+      mkDerivation {
+        pname = "myaml";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          aeson base bytestring directory filepath optparse-applicative text
+          unordered-containers vector yaml
+        ];
+        homepage = "https://github.com/yamadapc/myaml";
+        description = "Manipulate YAML with a UNIX tool";
+        license = stdenv.lib.licenses.mit;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
